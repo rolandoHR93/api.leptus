29b2018fd1f824394079396f383ea9c6e9a51b18
Rolando HR <rolando167@hotmail.com>
2021-11-16 21:46:48 -0500

    web, Login Test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -4,19 +4,53 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
+
+    /** @test */
+	public function a_post_can_be_created()
+	{
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/api/usuarios/register', [
+            "name" => "Lucia",
+			"email" => "test5@test.com",
+			"password" => "lima2021",
+		]);
 
         $response->assertStatus(200);
     }
+
+    /** @test */
+	public function a_post_can_be_login()
+	{
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/api/usuarios/login', [
+            "name" => "Lucia",
+			"email" => "test5@test.com",
+			"password" => "lima2021",
+		]);
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+	public function a_post_can_be_info()
+	{
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/api/usuarios/userinfo', [
+            "name" => "Lucia",
+			"email" => "test5@test.com",
+			"password" => "lima2021",
+		]);
+
+        $response->assertStatus(200);
+    }
+
 }
