3a0869c4593827f6687f0249b4357095d3c0e570
Rolando HR <rolando167@hotmail.com>
2021-12-06 20:34:19 -0500

    Modelos

---
+++ app/Models/Externo/Reportes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Externo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reportes extends Model
+{
+    use HasFactory;
+}
