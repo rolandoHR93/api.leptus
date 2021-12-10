ba5ee1b0c98e7e6e11c1f83b54460ac19cc6e051
Rolando HR <rolando167@hotmail.com>
2021-12-10 15:44:41 -0500

    welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>Laravel</title>
+		<title>Api - Leptus</title>
 
 		<!-- Fonts -->
 		<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,12 +18,20 @@
 			body {
 				font-family: 'Nunito', sans-serif;
 			}
-            .colorOrage{
+            .colorOrange{
                 color: #EF3B2D;
             }
 
             .colorBlue{
                 color: #0070ff;
+            }
+            /*
+            Set the color of the icon
+            https://codepen.io/aurer/pen/jEGbA
+            */
+            svg.svgAnimate path,
+            svg rect{
+            fill: #EF3B2D;
             }
 		</style>
 	</head>
@@ -32,7 +40,19 @@
 
 			<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 				<div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1 class="colorOrage">API - {{date('d/m/Y')}}
+
+                    <svg class="svgAnimate" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                        <rect x="0" y="0" width="4" height="10" fill="#333">
+                        <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0" dur="0.6s" repeatCount="indefinite"></animateTransform>
+                        </rect>
+                        <rect x="10" y="0" width="4" height="10" fill="#333">
+                        <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0.2s" dur="0.6s" repeatCount="indefinite"></animateTransform>
+                        </rect>
+                        <rect x="20" y="0" width="4" height="10" fill="#333">
+                        <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0.4s" dur="0.6s" repeatCount="indefinite"></animateTransform>
+                        </rect>
+                    </svg>
+                    <h1 class="colorOrange">API - {{date('d/m/Y')}}
                     </h1>
 				</div>
 
@@ -67,7 +87,7 @@
 						<div class="p-6 border-t border-gray-200 dark:border-gray-700">
 							<div class="flex items-center">
 								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-								<div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline colorBlue">Laravel News</a></div>
+								<div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline dark:text-white">Laravel News</a></div>
 							</div>
 
 							<div class="ml-12">
@@ -80,7 +100,7 @@
 						<div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
 							<div class="flex items-center">
 								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-								<div class="ml-4 text-lg leading-7 font-semibold colorOrage underline">Api - 2021</div>
+								<div class="ml-4 text-lg leading-7 font-semibold colorOrange underline">Api - 2021</div>
 							</div>
 
 							<div class="ml-12">
