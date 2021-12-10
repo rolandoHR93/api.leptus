044a5e49378e0299778fa0cf38c3dc2680731ce8
Rolando HR <rolando167@hotmail.com>
2021-12-10 15:29:55 -0500

    ca

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,14 +18,21 @@
 			body {
 				font-family: 'Nunito', sans-serif;
 			}
+            .colorOrage{
+                color: #EF3B2D;
+            }
+
+            .colorBlue{
+                color: #0070ff;
+            }
 		</style>
 	</head>
 	<body class="antialiased">
 		<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
 			<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-				<div class="flex justify-center pt-8 sm:justify-start sm:pt-0" style="color: #EF3B2D">
-                    <h1>API - {{date('d/m/Y')}}
+				<div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                    <h1 class="colorOrage">API - {{date('d/m/Y')}}
                     </h1>
 				</div>
 
@@ -60,7 +67,7 @@
 						<div class="p-6 border-t border-gray-200 dark:border-gray-700">
 							<div class="flex items-center">
 								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-								<div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline text-gray-900 dark:text-white">Laravel News</a></div>
+								<div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline colorBlue">Laravel News</a></div>
 							</div>
 
 							<div class="ml-12">
@@ -73,7 +80,7 @@
 						<div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
 							<div class="flex items-center">
 								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-								<div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Api - 2021</div>
+								<div class="ml-4 text-lg leading-7 font-semibold colorOrage underline">Api - 2021</div>
 							</div>
 
 							<div class="ml-12">
