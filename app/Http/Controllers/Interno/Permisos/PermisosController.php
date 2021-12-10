640134d6e0c2ae432bc4b05a5eacc52968cc61d7
ELofycial <larosatoro979@gmail.com>
2021-12-10 10:22:46 -0500

    CAPermisos_Controler

--- app/Http/Controllers/Interno/Permisos/PermisosController.php
+++ app/Http/Controllers/Interno/Permisos/PermisosController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Permisos;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\PermisosRepository;
 use Illuminate\Http\Request;
 
 class PermisosController extends Controller
 {
-    //
+    protected $repository;
+
+    public function __construct(PermisosRepository $repository)
+    {
+        $this->middleware('verifyApiCode');
+        $this->repository = $repository;
+    }
+
+    public function lista($key)
+    {
+        try {
+
+            $respuesta = $this->repository->lista();
+
+			return response()->json(['data' => $respuesta], 200);
+		}
+		catch (Exception $e) {
+			return response()->json(["error" => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+		}
+    }
 }
