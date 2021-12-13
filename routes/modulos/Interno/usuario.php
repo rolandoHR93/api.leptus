2b9e5e950ccfc1b9e8413e80cdb81243d851b70f
Rolando HR <rolando167@hotmail.com>
2021-12-13 09:41:42 -0500

    cambio

--- routes/modulos/Interno/usuario.php
+++ routes/modulos/Interno/usuario.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\Users\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+// http://127.0.0.1:8000/api/usuarios/
+// --------------
+
 // Route::post("/userinfo",  [AuthController::class, 'userinfo'])->middleware('auth:sanctum');
+
 
 Route::get('/', function(){
 	return response()->json('--- Usuarios ---', 200);
