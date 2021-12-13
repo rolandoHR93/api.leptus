b44bb38123f7b916b12d9c3f072094885e035ee2
Rolando HR <rolando167@hotmail.com>
2021-12-13 15:53:02 -0500

    ca

--- app/Models/Interno/ServiciosModel.php
+++ app/Models/Interno/ServiciosModel.php
@@ -7,27 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class ServiciosModel extends Model
 {
-    use HasFactory;
-    protected $table = 'leptus.servicios';
-    protected $primaryKey = 'id';
+	use HasFactory;
+	protected $table = 'leptus.servicios';
+	protected $primaryKey = 'id';
 
-    protected $fillable = [
-        'nombre_servicio',
-        'descripcion',
-        'nombre_clave',
-        'precio',
-        'stock',
-        'flag',
-        'delete_comentario',
-        'delete',
-        'state',
-        'created_by',
-        'updated_by',
-        'deleted_by',
-        'deleted_at',
-    ];
+	protected $fillable = [
+		'nombre_servicio',
+		'descripcion',
+		'nombre_clave',
+		'precio',
+		'stock',
+		'flag',
+		'delete_comentario',
+		'delete',
+		'state',
+		'created_by',
+		'updated_by',
+		'deleted_by',
+		'deleted_at',
+	];
 
-    public function getDateFormat() {
-        return 'Y-d-m H:i:s';
-    }
+	public function getDateFormat() {
+		return 'Y-d-m H:i:s';
+	}
 }
