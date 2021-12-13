667be4665ae4de98ac4ec130889f5fda781ebea9
Rolando HR <rolando167@hotmail.com>
2021-12-13 15:13:46 -0500

    ca

--- app/Models/Interno/PersonalAccessTokens.php
+++ app/Models/Interno/PersonalAccessTokens.php
@@ -2,13 +2,31 @@
 
 namespace App\Models\Interno;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-class PersonalAccessTokens extends Model
+class PersonalAccessTokens extends SanctumPersonalAccessToken
 {
-    use HasFactory;
     protected $table = 'dbo.personal_access_tokens';
+
+     /**
+     * Limit saving of PersonalAccessToken records
+     *
+     * We only want to actually save when there is something other than
+     * the last_used_at column that has changed. It prevents extra DB writes
+     * since we aren't going to use that column for anything.
+     *
+     * @param  array  $options
+     * @return bool
+     */
+    public function save(array $options = [])
+    {
+        $changes = $this->getDirty();
+        // Check for 2 changed values because one is always the updated_at column
+        if (! array_key_exists('last_used_at', $changes) || count($changes) > 2) {
+            parent::save();
+        }
+        return false;
+    }
 
     public function getDateFormat() {
         return 'Y-m-d H:i:s';
