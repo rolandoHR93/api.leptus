32be53cf6b44fb43bef09534c55e3ef70c913f49
Rolando HR <rolando167@hotmail.com>
2021-12-13 11:42:03 -0500

    ca

--- app/Models/Interno/User.php
+++ app/Models/Interno/User.php
@@ -12,7 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'users';
+    protected $table = 'dbo.users';
+    // public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
