49038d0ea1252f1b68f8aa9760d4dea8fbcbbbd8
Rolando HR <rolando167@hotmail.com>
2021-12-16 09:19:25 -0500

    Mail cambios

---
+++ app/Mail/Externo/PedidoDeliveryMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail\Externo;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PedidoDeliveryMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
