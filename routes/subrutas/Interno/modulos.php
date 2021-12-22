ed4b5e671e947f8e036397e7e61e7f5af5e5147b
Rolando HR <rolando167@hotmail.com>
2021-12-22 18:40:25 -0500

    CA

--- routes/subrutas/Interno/modulos.php
+++ routes/subrutas/Interno/modulos.php
@@ -6,14 +6,16 @@ use Illuminate\Support\Facades\Route;
 // http://127.0.0.1:8000/api/modulos/
 //----------------
 
+//CRUD MODULOS
 Route::get("lista/{key}",  [ModulosController::class, 'lista']);
 Route::post("create/{key}",  [ModulosController::class, 'create']);
 Route::put("update/{key}/{id}",  [ModulosController::class, 'update']);
 Route::delete("delete/{key}/{id}",  [ModulosController::class, 'delete']);
 
-
+//RELACION MODULOS-ITEMACCESO
 Route::post("agregarModuloItem/{key}",  [ModulosController::class, 'agregarModuloItem']);
 Route::delete("deleteModuloItem/{key}",  [ModulosController::class, 'deleteModuloItem']);
 
+//RELACION MODULOS-PERSONA
 Route::post("agregarModuloPersona/{key}",  [ModulosController::class, 'agregarModuloPersona']);
 Route::delete("deleteModuloPersona/{key}",  [ModulosController::class, 'deleteModuloPersona']);
