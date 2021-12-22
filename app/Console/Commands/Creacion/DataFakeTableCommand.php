c118f0c0aec25b6b29cfcfc734085c46db1f29f5
Rolando HR <rolando167@hotmail.com>
2021-12-22 16:02:29 -0500

    cambios

---
+++ app/Console/Commands/Creacion/DataFakeTableCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Console\Commands\Creacion;
+
+use App\Models\Interno\Modulos;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class DataFakeTableCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'creacion:dataFakeTable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generar Data Fake en la BD, para pruebas.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // $model->getFillable()
+
+        // --------------------
+        echo "\n---- Generando Data Fake ∞∞∞∞∞∞∞∞∞∞∞∞";
+        // --------------------
+        $modulo = new Modulos();
+
+        for ($i=3; $i < 20; $i++) {
+            $modulo->create([
+                'nombre' => "Modulo ${i}"
+                , 'descripcion' => "Descripcion ".Str::random(4)
+                , 'tipo_id' => 1
+                , 'created_by' => 1
+            ]);
+        }
+
+        // --------------------
+        echo "\n√√√ EXITO ⁞⁞_⁞⁞ √√√√√√√";
+        // --------------------
+
+        return Command::SUCCESS;
+    }
+
+    public function ejemplo()
+    {
+         // $model->getFillable()
+
+        // --------------------
+        echo "\n---- Generando Data Fake ∞∞∞∞∞∞∞∞∞∞∞∞";
+        // --------------------
+        $modulo = new Modulos();
+
+        for ($i=3; $i < 20; $i++) {
+            $modulo->create([
+                'nombre' => "Modulo ${i}"
+                , 'descripcion' => "Descripcion ".Str::random(4)
+                , 'tipo_id' => 1
+                , 'created_by' => 1
+            ]);
+        }
+
+        // --------------------
+        echo "\n√√√ EXITO ⁞⁞_⁞⁞ √√√√√√√";
+        // --------------------
+
+        return Command::SUCCESS;
+    }
+}
