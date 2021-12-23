deabd3601d2f2036b83105c053aafb3929f4f768
Rolando HR <rolando167@hotmail.com>
2021-12-23 08:55:03 -0500

    Procfile

--- routes/subrutas/Externo/clientesAdministrador.php
+++ routes/subrutas/Externo/clientesAdministrador.php
@@ -2,3 +2,8 @@
 
 use App\Http\Controllers\GrupoServicios\FacturacionSoftwareController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function(){
+	return response()->json('--- Administrador ---', 200);
+});
