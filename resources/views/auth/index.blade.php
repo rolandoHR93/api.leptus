4192bb6bced15affc0a41074f2087715b57a64b8
Rolando HR <rolando167@hotmail.com>
2021-12-29 14:08:03 -0500

    ca

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -143,6 +143,10 @@
 					<div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
 						Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
 					</div>
+			        <div class="ml-4 leading-7 font-semibold">
+                        <a href="/api" class="underline text-gray-700">Api Home</a>
+                    </div>
+
 				</div>
 			</div>
 		</div>
