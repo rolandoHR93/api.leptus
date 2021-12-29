6b4976606067edf1fbb8e047a6fb10a005685c69
Rolando HR <rolando167@hotmail.com>
2021-12-29 11:28:14 -0500

    ca

--- routes/subrutas/Interno/authRoute.php
+++ routes/subrutas/Interno/authRoute.php
@@ -6,14 +6,13 @@ use Illuminate\Http\Request;
 
 Route::post("/register/{key}",  [AuthController::class, 'register']);
 Route::post("/login/{key}",     [AuthController::class, 'login']);
-Route::get("/loginSE/{key}",     [AuthController::class, 'loginSE']);
 Route::get("/loginSE2/{key}",     [AuthController::class, 'loginSE']);
 
 
-// Route::get('/loginSE', function(){
-//     return view('welcome');
-// 	// return response()->json('--- Bienvenido  👍 ---', 200);
-// });
+Route::get('/loginSE/{key}', function(){
+    return view('auth.index');
+	// return response()->json('--- Bienvenido  👍 ---', 200);
+});
 
 //using middleware
 Route::group(['middleware' => ['auth:sanctum']], function () {
